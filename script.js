a0bda49d71e480e9a777b65e8eef2c7629064055
kirtijaiswal01 <85224179+kirtijaiswal01@users.noreply.github.com>
2022-06-11 10:54:28 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+let weather = {
+     "apiKey": "17b4b444e1d681387f66cbbea414ddeb",
+     fetchWeather: function(city) {
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q="  +  city  + "&appid=" + this.apiKey
+         ).then((response) => response.json())
+          .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function(data) {
+       const { name } = data;
+       const { icon, description } = data.weather[0];
+       const { temp, humidity }  = data.main;
+       const { speed } = data.wind;
+       document.querySelector(".city").innerText = "Weather in " + name;
+       document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png"
+       document.querySelector(".description").innerText= description;
+       document.querySelector(".temp").innerText = temp +"K";
+       document.querySelector(".humidity").innerText = "Humidity: "+ humidity + "%";
+       document.querySelector(".wind").innerText = "wind speed: "+ speed + "km/h";
+       document.querySelector(".weather").classList.remove("loading");
+       document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name +  "')"
+    },
+    search: function() {
+        this.fetchWeather(document.querySelector(".search-bar").value);
+    }
+};
+document
+.querySelector(".search button")
+.addEventListener("click", function () {
+   weather.search();
+});
+
+document.querySelector(".search-bar").addEventListener("keyup", function (event) {
+   if (event.key == "Enter") {
+       weather.search();
+   }
+});
+
+weather.fetchWeather("Varanasi");
